39f15e56384c84ea0459cc205444be0868612cdd
TheHalfGhost <Alexander.THG@knights.ucf.edu>
2021-02-05 18:17:11 -0500

    first commit

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+    public Text Counttext;
+    public float Down = 4;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Down -= Time.deltaTime;
+
+        if(Down <= 4)
+        {
+            Counttext.text = "3";
+        }
+        if(Down <= 3)
+        {
+            Counttext.text = "2";
+        }
+        if (Down <= 2)
+        {
+            Counttext.text = "1";
+        }
+        if(Down <= 1)
+        {
+            Counttext.text = "GO!";
+        }
+        if(Down <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
